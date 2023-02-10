d0e47631cd38e786e44b33dfe910dcade3f002ab
Paulmue0 <muellerpaul2000@live.de>
2023-02-10 02:41:04 +0100

    refactored webscraper

--- src/backend/scraper.py
+++ src/backend/scraper.py
@@ -7,19 +7,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from storage import Storage
-from storage import RequestResult
+from storage import QueryType
 from item import *
 import time
+import math
 
 
 class Scraper:
-    def __init__(self, storage: Storage) -> None:
-        self.storage = storage
+    def __init__(self, store: Storage) -> None:
+        self.store = store
         self.driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()))
 
     def get_price(self, product: Request) -> float:
-        self.set_driver_path(self.search_query(product['name']))
+        self.set_driver_path(self.search_query(product))
         try:
             price_euros = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located(
@@ -27,6 +28,7 @@ class Scraper:
             )
         except:
             self.driver.quit()
+            return math.inf
 
         try:
             price_cents = WebDriverWait(self.driver, 10).until(
@@ -35,11 +37,15 @@ class Scraper:
             )
         except:
             self.driver.quit()
-        print(f"Der Preis ist heiß: nur {price_euros.text},{price_cents.text}\n")
-        return f"{price_euros.text},{price_cents.text}"
+            return math.inf
+        price = str(price_euros.text).replace(".", "")
+        price += "."
+        price += str(price_cents.text)
+        print(f"Der Preis ist heiß: nur {price}€uronen\n")
+        return float(price)
 
     def get_cheapest_offer_url(self, product: Item) -> str:
-        self.set_driver_path(self.search_query(product['name']))
+        self.set_driver_path(self.search_query(product))
         try:
             cheapest_url = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located(
@@ -47,18 +53,23 @@ class Scraper:
             )
         except:
             self.driver.quit()
-        # self.driver.find_element(By.XPATH('/html/body/div[1]/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div[1]/div[3]/div/div[1]/div[2]')).click();
-        # cheapest_url.click()
+            return ''
         link = f"https://www.preissuchmaschine.de{cheapest_url.get_attribute('href')}"
-        self.driver.get(link)
-        time.sleep(3)
+        try:
+            self.driver.get(link)
+        except:
+            return ''
+        try:
+            definitive_url = WebDriverWait(self.driver, 10).until(EC.url_changes(link))
+        except:
+            self.driver.quit()
+            return ''
+        
         url = self.driver.current_url
-        # print(cheapest_url)
-        # print(link)
-        print(url)
+        return str(url)
 
     def search_query(self, product: Item) -> str:
-        query_name = product.lstrip().replace(" ", "-")
+        query_name = product['name'].lstrip().replace(" ", "-")
         return (f'https://www.preissuchmaschine.de/Katalog/Suche/{query_name}.html')
 
     def set_driver_path(self, url):
@@ -66,12 +77,15 @@ class Scraper:
 
     def perform_scraping(self) -> None:
         # Perform scraping for NEW products
-        items = self.storage.get_data(RequestResult.REQUESTS)
-        for product in items[Condition.NEW.value]:
-            self.set_driver_path(self.search_query(product['name']))
+        items = self.store.get_data(QueryType.REQUESTS)
+        for requested_product in items[Condition.NEW.value]:
+            self.set_driver_path(self.search_query(requested_product))
 
-            queried_product = Result(product['name'], Condition.NEW, self.get_price(
-                product), False, self.get_cheapest_offer_url(product))
+            queried_product = Result(name=requested_product['name'], type=Condition.NEW, price=self.get_price(
+                requested_product), url=self.get_cheapest_offer_url(requested_product))
+            
+            if queried_product.price <= requested_product['threshold']:
+                self.store.add(queried_product)
 
             self.driver.quit()
 

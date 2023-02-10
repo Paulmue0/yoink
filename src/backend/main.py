63d092871f6eaba7e617eb6df48df35a4db698df
Paulmue0 <muellerpaul2000@live.de>
2023-02-10 02:41:50 +0100

    testing

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,20 @@
 from storage import Storage
-from item import Request
-from condition import Condition
-from storage import RequestResult
+from item import Request, Result
+from enums import Condition
+from storage import QueryType
 from scraper import Scraper
+from notifier import Notifier
 
-new_item = Request('LG 38WN95c', Condition.NEW, 400)
-second_item = Request('LG 38WN959c', Condition.NEW, 900)
+first_item = Request(name='Nothing Phone', type=Condition.NEW, threshold=500)
+second_item = Request(name='LG 38WN959c', type=Condition.NEW, threshold=900)
 
 store = Storage()
+# scrap = Scraper(store)
+# scrap.perform_scraping()
+# store.save()
 
-scrape = Scraper(store)
-scrape.perform_scraping()
 
+telegram_bot = Notifier()
+# telegram_bot.start_polling()
+# telegram_bot.send_message("wie gehts")
+# store.save()
